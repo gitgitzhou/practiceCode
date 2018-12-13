74efc0c8836620e229bb0fd505eca20be6a9e684
Jianfeng Zhou <feng@Jianfengs-Air.lan>
2018-12-13 22:54:33 +0100

    object and class in python

--- python/object_2.py
+++ python/object_2.py
@@ -3,8 +3,8 @@
 class Point:
 
    def __init__( self, x=0, y=0):
-      self.x = x
-      self.y = y
+      self.X = x
+      self.Y = y
       class_name = self.__class__.__name__
       print class_name, "constructed"
 
@@ -19,3 +19,5 @@ print id(pt1), id(pt2), id(pt3) # prints the ids of the obejcts
 del pt1
 del pt2
 del pt3
+pt4 = Point(3, 4)
+print "x value in pt4 is : %d" % pt4.X
