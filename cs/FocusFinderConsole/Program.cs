86ec839b9eabb8d384165be256cdc05e601a553e
Jianfeng Zhou <jianfenz@axis.com>
2019-11-01 23:29:30 +0100

    Create c# class library dll and console app
    import the dll into console app
    dotnet new
    dotnet build
    dotnet run
    dotnet add reference
    dotnet publish
    
    Change-Id: I8be4f74e5e86e70faf7f1247aa58e04b70325e96

---
+++ cs/FocusFinderConsole/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FocusFinderLibrary;
+
+namespace ConsoleAppFocusFinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool moveFlag;
+            FocusFinder Finder = new FocusFinder();
+            Finder.SetFocusVal(100.0);
+            Finder.SetMotorPos(1.0);
+            Finder.PrintVal();
+            moveFlag = Finder.MoveMotor();
+            Console.WriteLine($"move motor flag: {moveFlag}");
+            Console.ReadKey();
+            
+        }
+    }
+}
---
+++ cs/FocusFinderLibrary/FocusFinder.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FocusFinderLibrary
+{
+    public class FocusFinder
+    {
+        public double focusValue;
+        public double motorPosition;
+
+        public void SetFocusVal(double focusVal)
+        {
+            focusValue = focusVal;
+        }
+        public void SetMotorPos(double motorPos)
+        {
+            motorPosition = motorPos;
+        }
+        public void PrintVal()
+        {
+            Console.WriteLine($"The current focus value is: {focusValue}");
+            Console.WriteLine($"The current motor position is: {motorPosition}");
+        }
+        public bool MoveMotor()
+        {
+            return true;
+        }
+    }
+}
