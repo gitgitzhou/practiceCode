ebfd04c4d2928fc7c23956c579cd8122ffae9973
Jianfeng Zhou <jianfenz@axis.com>
2019-11-07 13:53:52 +0100

    called python script in c# console
    Change-Id: Ie32489d737c37d633f854fab7bc2bb0cda4f84bb

---
+++ cs/callpython/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace callpython
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Console.WriteLine("Hello World!"); */
+            int x = 1;
+            int y = 2;
+            string progToRun = "/home/jianfenz/practiceCode/cs/callpython/hello.py";
+            char[] splitter = {'\r'};
+ 
+            Process proc = new Process();
+            proc.StartInfo.FileName = "python";
+            proc.StartInfo.RedirectStandardOutput = true;
+            proc.StartInfo.UseShellExecute = false;
+             
+            // call hello.py to concatenate passed parameters
+            proc.StartInfo.Arguments = string.Concat(progToRun, " ", x.ToString(), " ", y.ToString());
+            proc.Start();
+ 
+            StreamReader sReader = proc.StandardOutput;
+            string[] output = sReader.ReadToEnd().Split(splitter);
+ 
+            foreach (string s in output)
+                Console.WriteLine(s);
+ 
+            proc.WaitForExit();
+ 
+            Console.ReadLine();
+        }
+    }
+}
