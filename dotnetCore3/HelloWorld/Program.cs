dba0cf4789e21fb278008b8dc0e9561bf42cf50c
Jianfeng Zhou <jianfenz@axis.com>
2020-02-05 10:24:24 +0100

    donetCore3

---
+++ dotnetCore3/HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnetCore3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
